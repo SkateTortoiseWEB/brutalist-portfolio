3c47930b6af72870fdd7b5b6f43a1e939dd70fad
SkateTortoiseWEB <josephrobintruss@gmail.com>
2025-08-27 08:30:52 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['dl.dropbox.com', 'www.onlygfx.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dl.dropbox.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.onlygfx.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
